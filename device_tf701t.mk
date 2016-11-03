f59afae0c95032f6b533b15584ad3e5535e7cce5
timduru <github@timduru.org>
2016-11-03 18:18:44 +0100

    media_codecs

--- device_tf701t.mk
+++ device_tf701t.mk
@@ -45,6 +45,9 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/asus/tf701t/media/media_profiles.xml:system/etc/media_profiles.xml \
     device/asus/tf701t/media/media_codecs.xml:system/etc/media_codecs.xml \
     device/asus/tf701t/media/enctune.conf:system/etc/enctune.conf
